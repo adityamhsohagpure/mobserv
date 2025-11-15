c052838aeb00a7f8dea71a14e4be90b4494c3801
unknown <adityamhsohagpure1020@gmail.com>
2025-11-15 19:16:41 +0530

    change in postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createPost } = require("../controllers/postController");
 
-router.post("/upload", createPost);
+// IMPORT NAME MUST MATCH EXACT EXPORT NAME
+const { uploadPost } = require("../controllers/postController");
 
+// Test route (to confirm route is loading on Render)
+router.get("/test", (req, res) => {
+  res.send("Post Routes Working 🚀");
+});
+
+// Upload post API
+router.post("/upload-post", uploadPost);
 module.exports = router;
