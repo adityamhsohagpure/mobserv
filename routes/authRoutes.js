3313435b4fd1ae1804d95a01d227895a19c451f4
unknown <adityamhsohagpure999@gmail.com>
2026-02-22 13:23:30 +0530

    auth fix1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -90,8 +90,8 @@ router.post("/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.json({ token, user: {  userId: user._id,username: user.username, email: user.email } });
+    const token = jwt.sign({ id: user.userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    res.json({ token, user: {  userId: user.userId,username: user.username, email: user.email } });
   } catch (err) {
     res.status(500).json({ error: "Login error" });
   }
